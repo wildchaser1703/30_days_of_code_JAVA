61d7e0322a149039ac670d6e381b85081c7f5393
Toshali Mohapatra <53348090+wildchaser1703@users.noreply.github.com>
2021-02-07 14:35:02 +0530

    Add files via upload

---
+++ LetsReview.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        scn.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            String string = scn.nextLine();
+            char[] charArray = string.toCharArray();
+
+            for (int j = 0; j < charArray.length; j++) {
+                if (j % 2 == 0) {
+                    System.out.print(charArray[j]);
+                }
+            }
+
+            System.out.print(" ");
+
+            for (int j = 0; j < charArray.length; j++) {
+                if (j % 2 != 0) {
+                    System.out.print(charArray[j]);
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+
+}
