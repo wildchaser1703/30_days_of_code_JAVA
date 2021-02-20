856a4317039b1ef197269aab15302d9d65e21957
Toshali Mohapatra <53348090+wildchaser1703@users.noreply.github.com>
2021-02-20 19:49:41 +0530

    Update LetsReview.java

--- LetsReview.java
+++ LetsReview.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class LetsReview {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
