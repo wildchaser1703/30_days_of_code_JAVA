40eb177ab7b1e387b4c5d2a36f56320e099a42a5
Toshali Mohapatra <53348090+wildchaser1703@users.noreply.github.com>
2021-02-25 17:18:17 +0530

    Add files via upload

---
+++ runTimeandComplexity.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class runTimeandComplexity {
+    public static boolean isPrime(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn = new Scanner(System.in);
+        int T = scn.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int n = scn.nextInt();
+
+            if (n >= 2 && isPrime(n))
+                System.out.println("Prime");
+            else System.out.println("Not prime");
+        }
+
+        scn.close();
+    }
+}
